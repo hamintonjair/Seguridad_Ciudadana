870c9655533a937c775f60d35ee1cecb4912cab1
hamintonjair <hamintonjair@gmail.com>
2024-11-13 16:44:05 -0500

    se realizo cambios en los estilos del hover

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iniciar Sesión</title>
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style_login.css">
-    <link rel = "stylesheet"  href="<?php echo base_url();?>assets/css/bootstrap.min.css" >
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/libreria/sweetalert2/dist/sweetalert2.min.css">
 </head>
 
@@ -17,10 +17,11 @@
                 <div class="card">
                     <div class="card-header">Seguridad ciudadana - Iniciar Sesión</div>
                     <div class="card-body">
-                        <form >
+                        <form>
                             <div class="form-group">
                                 <label for="correo">Correo Electrónico</label>
-                                <input type="email" class="form-control valid validEmail" id="correo" name="correo" placeholder="Ingrese su correo">
+                                <input type="email" class="form-control valid validEmail" id="correo" name="correo"
+                                    placeholder="Ingrese su correo">
                             </div>
                             <div class="form-group">
                                 <label for="clave">Contraseña</label>
@@ -31,8 +32,7 @@
                         </form>
                         <p>¿No tienes una cuenta? <a
                                 href="<?php echo base_url(); ?>registro-nuevo-usuario">Regístrate</a></p>
-                                <p>Volver <a
-                                href="<?php echo base_url(); ?>">Principal</a></p>
+                        <p>Volver <a href="<?php echo base_url(); ?>">a la vista princpal</a></p>
 
                     </div>
                 </div>
@@ -43,9 +43,11 @@
 
 </body>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/libreria/sweetalert2/dist/sweetalert2.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/libreria/sweetalert2/dist/sweetalert2.min.js">
+</script>
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/login.js"></script>
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/functions_admin.js"></script>
+
 
 
 </html>
--- application/views/login/registro_usuario.php
+++ application/views/login/registro_usuario.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro de usuario</title>
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style_login.css">
     <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
@@ -17,21 +17,24 @@
                 <div class="card">
                     <div class="card-header">Registro de usuario</div>
                     <div class="card-body">
-                        <form enctype="multipart/form-data" >
+                        <form enctype="multipart/form-data">
                             <div class="form-group">
-                                <label for="nombre">Nombres y Apellodos</label>
-                                <input type="text" class="form-control valid validText" id="nombre" placeholder="Ingrese su nombre y apellidos">
+                                <label for="nombre">Nombres y Apellidos</label>
+                                <input type="text" class="form-control valid validText" id="nombre"
+                                    placeholder="Ingrese su nombre y apellidos">
                             </div>
                             <div class="form-group">
                                 <label for="correo">Correo Electrónico</label>
-                                <input type="email" class="form-control valid validEmail" id="correo" placeholder="Ingrese su correo">
+                                <input type="email" class="form-control valid validEmail" id="correo"
+                                    placeholder="Ingrese su correo">
                             </div>
                             <div class="form-group">
                                 <label for="clave">Contraseña</label>
                                 <input type="password" class="form-control" id="clave"
                                     placeholder="Ingrese su contraseña">
                             </div>
-                            <button type="button" onclick="registrarUsuario();" class="btn btn-primary">Registrarse</button>
+                            <button type="button" onclick="registrarUsuario();"
+                                class="btn btn-primary">Registrarse</button>
                         </form>
                         <p>¿Ya tienes una cuenta? <a href="<?php echo base_url(); ?>login">Inicia Sesión</a></p>
 
@@ -44,7 +47,8 @@
 
 </body>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/libreria/sweetalert2/dist/sweetalert2.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/libreria/sweetalert2/dist/sweetalert2.min.js">
+</script>
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/registrar_usuario.js"></script>
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/functions_admin.js"></script>
 
